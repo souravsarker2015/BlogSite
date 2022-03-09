2be0b5e2f9322a5b370345bca1f285322399bffd
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-03-09 23:57:51 +0600

    updated

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=500, error_messages=
-    {
+    title = models.CharField(max_length=500, error_messages={
         'max_length': "You Can't exceed more than 500 characters"
     })
     body = models.CharField(max_length=2000)
-    published_on = models.DateTimeField()
-    last_updated_on = models.DateTimeField()
-    slug = models.SlugField()
-    status = models.CharField()
+    published_on = models.DateTimeField(auto_now_add=True)
+    last_updated_on = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=300, unique=True, null=True, blank=True)
+    status = models.CharField(max_length=500)
