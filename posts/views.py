ecfd613be1b03577f3913eeecc3df472bf63d5a8
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-03-14 22:12:57 +0600

    updated

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+Updating Data in the database
+for comment in Comment.objects.all():
+... print(comment.id)
+>>> cmt2 = Comment.objects.get(id=......)
+>>> cmt2.post
+<Post: My Educational Post 2>
+>>> cmt2.post = p1
+>>> cmt2.save()
+cmt2.post
+<Post: My Educational Post 1>
+"""
